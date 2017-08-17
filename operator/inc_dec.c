d3649d8190ca9976d0db3f4094a338e5ed2caef6
Masaki Abe <abem@dc4.so-net.ne.jp>
2017-08-17 11:38:06 +0900

    演算子の挙動確認。後置・前置での動作を確認。

--- operator/inc_dec.c
+++ operator/inc_dec.c
@@ -7,12 +7,17 @@ int main()
 	int a,b,c,d;
 
 	a=++i + ++i;
+printf("ai=%d\n", i);
 	i=0;
-	b=++i + i++;
+	//b=++i + i++;
+	b=++i + i;
+printf("bi=%d\n", i);
 	i=0;
 	c=i++ + ++i;
+printf("ci=%d\n", i);
 	i=0;
 	d=i++ + i++;
+printf("di=%d\n", i);
 	i=0;
 
 	printf("%d,%d,%d,%d\n",a,b,c,d);
